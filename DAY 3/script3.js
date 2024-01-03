885359d9471c71564dd4c9d12857b110693e6a6e
Mayur <mayursawant9028@gmail.com>
2024-01-03 22:05:11 +0530

    practise files

---
+++ DAY 3/script3.js
@@ -0,0 +1,10 @@
+let path ="MAYUR SAWANT ";
+console.log(path);
+
+let num1=10;
+let num2=5;
+let result=num1+num2;
+
+console.log("type of num1 is: "+ typeof(num1));
+console.log("type of num2 is :"+typeof(num2));
+
---
+++ day 2/script.js
@@ -0,0 +1,44 @@
+//console.log("from html file")
+// alert("hello")
+// console.error("error observed")
+// console.error("error observed")
+// this is single line comment --> comment 
+
+/* second way of comment */
+
+/* contrl + / --> keyboard shortcut */
+// control + > arrow --> to shift from word to word
+// control+ d  --> to shift from line to line 
+// shift + alt + arrow --> to copy line 
+// control + ~ --> to open terminal 
+
+                                                   /* VARIABLE */
+
+ /*  1] VAR 
+     2] LET 
+     3] CONST  
+     
+     */
+     let variable_name = "value";
+     const first_name = "Mayur";   
+     const last_name = "Sawant";
+     document.getElementById("firstheading");
+     firstheading.innerHTML = `Hello ALL My Details are ${first_name} ${last_name}`;
+     age = 25;
+     const designation = "QA ANALYST"   
+     const full_name = first_name+last_name;
+     const number = 23+23;   
+     console.log(number);
+     console.log(full_name);
+
+     /*                                              DATA TYPES
+
+     1] PRIMITIVE DATA TYPES [ STORED IN STACK MEMORY ]
+     2] NON PRIMITIVE DATA TYPES [ STORED IN HIP MEMORY ]
+     3]
+
+
+                                                                                                            */
+
+     
+     
