ab4c61679c6e899efc877ebf8f54f1f37c3c084c
Mayur <mayursawant9028@gmail.com>
2024-01-08 21:51:41 +0530

    day 7 changes

---
+++ DAY 4/script4.js
@@ -0,0 +1,8 @@
+console.log("ARRAY PRACTISED");
+
+let fruitlist = ["apple" , "orange" , "pinapple"];
+//console.log(fruitlist);
+fruitlist.push("12");  // insert new element at end of an array 
+fruitlist.unshift("mayur");
+console.log(fruitlist);
+console.log(fruitlist.indexOf[0]);
